3cdc463745694a3ef6c92b96ec5e8d84590e2aec
Heru Handika <herubiolog@gmail.com>
2021-02-19 09:45:38 -0600

    Add type hints.

--- ecc/main.py
+++ ecc/main.py
@@ -1,11 +1,13 @@
+from typing import List
+
 import click
 
 from ecc import utils
 
 @click.command()
 @click.option('--dir', '-d', help="Add dir to the files")
-def main(dir):
-    fpath = dir + "/*.xlsx"
-    files = utils.find_files(fpath)
+def main(dir: str):
+    fpath: str = dir + "/*.xlsx"
+    files: List[str] = utils.find_files(fpath)
     utils.convert_excel_to_csv(files)
     print("DONE!")
--- ecc/utils.py
+++ ecc/utils.py
@@ -1,17 +1,13 @@
 from glob import glob
+from typing import List
 
 import pandas as pd
 
-def find_files(filepath: str):
+def find_files(filepath: str) -> List[str]:
     return glob(filepath)      
 
 
-def convert_excel_to_csv(filenames):
-    """Batch converting excel files to csv.
-
-    Args:
-        filepath (string): Use wildcard to match filenames.
-    """
+def convert_excel_to_csv(filenames: List[str]):
     for excel in filenames:
         out_filenames = excel.replace('.xlsx','.csv')
         dataframe = pd.read_excel(excel, engine='openpyxl')
